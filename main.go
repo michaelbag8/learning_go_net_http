aaad9b7f6f26ea99786ebd399eced559624a738c
michaelbag8 <michaelbag8@gmail.com>
2026-05-20 17:10:43 +0100

    running local server

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Home Page"))
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("About Page"))
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Contact Page"))
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request){
+	name := r.URL.Query().Get("param")
+
+	lang := r.URL.Query().Get("param")
+
+	if name == ""{
+		name = "Guest!"
+	}
+
+	if lang == "es"{
+		fmt.Fprintln(w, "Hello %v", name)
+	}else{
+		fmt.Fprintf(w, "Hello %v", name)
+	}
+	
+}
+
+func main(){
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contact", contactHandler)
+
+	http.HandleFunc("/greet", greetHandler)
+
+	if err := http.ListenAndServe(":9090", nil); err != nil{
+		log.Fatal("Server failed to start: ",err)
+	}
+}
