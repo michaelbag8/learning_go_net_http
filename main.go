a44531e2544dc2d43bed7c61b2b330a8f84f033f
michaelbag8 <michaelbag8@gmail.com>
2026-05-20 21:05:10 +0100

    Add:getUserID function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request){
@@ -24,23 +25,57 @@ func greetHandler(w http.ResponseWriter, r *http.Request){
 	lang := r.URL.Query().Get("lang")
 
 	if name == ""{
-		name = "Guest!"
+		name = "Guest"
 	}
 
 	if lang == "es"{
-		fmt.Fprintf(w, "Halo %v", name)
+		fmt.Fprintf(w, "Hola %s", name)
 	}else{
-		fmt.Fprintf(w, "Hello %v", name)
+		fmt.Fprintf(w, "Hello %s", name)
 	}
 	
 }
+
+func farewellHandler(w http.ResponseWriter, r *http.Request){
+	name := r.URL.Query().Get("name")
+
+	lang := r.URL.Query().Get("lang")
+
+	if name == ""{
+		name = "Guest"
+	}
+
+	if lang == "es"{
+		fmt.Fprintf(w, "Adiós, %s", name)
+	}else{
+		fmt.Fprintf(w, "Goodbye, %s", name)
+	}
+}
+
+
+func getUserIdHandler(w http.ResponseWriter, r *http.Request){
+	path :=r.URL.Path
+
+	parts := strings.Split(path, "/")
+
+	if len(parts) < 3{
+		http.Error(w, "User ID not provided", http.StatusBadRequest)
+		return
+	}
+
+	userID := parts[2]
+	fmt.Fprintf(w, "User ID: %s", userID)
+}
+
+
 
 func main(){
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/contact", contactHandler)
-
+	http.HandleFunc("/farewell", farewellHandler)
 	http.HandleFunc("/greet", greetHandler)
+	http.HandleFunc("/users/", getUserIdHandler)
 
 	if err := http.ListenAndServe(":9090", nil); err != nil{
 		log.Fatal("Server failed to start: ",err)
